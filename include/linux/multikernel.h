e930c0017a8e5c3998e651bbcf92f4ca57bdef94
truesamsepiol <true.samsepiol@gmail.com>
2026-08-20 13:43:50 +0000

    j'ai oublié d'ajouter ce fichier dans le precedent commit

--- include/linux/multikernel.h
+++ include/linux/multikernel.h
@@ -435,7 +435,8 @@ struct mk_instance {
 	char *name;                     /* User-provided instance name */
 	enum mk_instance_state state;   /* Current state */
 
-	//EO -> 1 ajouter le chemin vers les outputs du test kernel
+	//EO -> output
+	char *output; /* fichier contenant la sortie stdout du test kernel */ 
 
 	/* Resource management - list of reserved memory regions */
 	struct list_head memory_regions;  /* List of struct mk_memory_region */
